cf48fdafac6a6ee48f111cba330ff4d5e552f0fa
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-07-21 21:05:03 +0545

    added an overlay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react/cjs/react.production.min";
+import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,22 +1,27 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-  const CartItems = <ul className={classes['cart-items']}>{[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(
-    (item) => <li>{item.name}</li>
-  )}</ul>;
+  const CartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
   return (
-    <div>
+    <Modal>
       {CartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 

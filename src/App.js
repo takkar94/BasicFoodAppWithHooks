1640fca6faff2d57cc0e8f273d02d1b27f458e94
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-08-04 14:53:50 +0545

    using usereducer to mannage context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Fragment } from "react/cjs/react.production.min";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
@@ -15,7 +14,6 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   }
-
 
   return (
     <CartProvider>
--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,16 +1,27 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCardButton.module.css'
 
 
 const HeaderCardButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
+        return (
+            curNumber + item.amount
+        )
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
-                <CartIcon></CartIcon>
+                <CartIcon/>
             </span>
             <span> Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )

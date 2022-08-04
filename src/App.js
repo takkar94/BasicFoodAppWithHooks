0574a93291e7056d2b960d0e74f18da125c7a599
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-08-04 14:30:06 +0545

    added store for the context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Fragment } from "react/cjs/react.production.min";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -17,13 +18,13 @@ function App() {
 
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart = {showCartHandler}/>
       <main>
         <Meals onHideCart = {hideCartHandler}/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 

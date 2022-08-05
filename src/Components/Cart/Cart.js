6308352eeb3692b024b19e173344e13a919ce068
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-08-05 14:22:07 +0545

    added a pointer functions for carts

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,21 +1,30 @@
 import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
-import CartContext from '../../store/cart-context';
-
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 const Cart = (props) => {
-
-  const cartCtx = useContext(CartContext)
+  const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length >0 
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (id) => {};
+
   const CartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id )}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
-
   );
 
   return (
@@ -26,7 +35,9 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
